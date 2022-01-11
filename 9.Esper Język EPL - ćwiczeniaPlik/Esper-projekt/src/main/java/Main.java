f9a9a7764f89c0eef3458bcd2783350f9a568bb8
Wojciech Szczepaniak <wszczepaniak978@gmail.com>
2022-01-11 23:14:11 +0100

    9

---
+++ 9.Esper Język EPL - ćwiczeniaPlik/Esper-projekt/src/main/java/Main.java
@@ -0,0 +1,47 @@
+
+import com.espertech.esper.common.client.EPCompiled;
+import com.espertech.esper.common.client.configuration.Configuration;
+import com.espertech.esper.compiler.client.CompilerArguments;
+import com.espertech.esper.compiler.client.EPCompileException;
+import com.espertech.esper.compiler.client.EPCompilerProvider;
+import com.espertech.esper.runtime.client.*;
+
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Configuration configuration = new Configuration();
+        configuration.getCommon().addEventType(KursAkcji.class);
+        EPRuntime epRuntime = EPRuntimeProvider.getDefaultRuntime(configuration);
+
+        EPDeployment deployment = compileAndDeploy(epRuntime,
+                "select istream data, spolka, obrot" +
+                    " from  KursAkcji(market= 'NYSE').win:ext_timed_batch(data.getTime(), 7 days) " +
+                    " order by obrot desc " +
+                        "LIMIT 3");
+
+        ProstyListener prostyListener = new ProstyListener();
+        for (EPStatement statement : deployment.getStatements()) {
+            statement.addListener(prostyListener);
+        }
+
+        InputStream inputStream = new InputStream();
+        inputStream.generuj(epRuntime.getEventService());
+    }
+    public static EPDeployment compileAndDeploy(EPRuntime epRuntime, String epl) {
+        EPDeploymentService deploymentService = epRuntime.getDeploymentService();
+        CompilerArguments args = new CompilerArguments(epRuntime.getConfigurationDeepCopy());
+        EPDeployment deployment;
+        try {
+            EPCompiled epCompiled = EPCompilerProvider.getCompiler().compile(epl, args);
+            deployment = deploymentService.deploy(epCompiled);
+        } catch (EPCompileException e) {
+            throw new RuntimeException(e);
+        } catch (EPDeployException e) {
+            throw new RuntimeException(e);
+        } return deployment;
+    }
+
+}
